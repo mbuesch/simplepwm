e287158903705536313def0348366b0ab000c1de
Michael Buesch <m@bues.ch>
2018-05-01 17:43:18 +0200

    Implement high resolution PWM for low ranges
    Signed-off-by: Michael Buesch <m@bues.ch>

--- firmware/main.c
+++ firmware/main.c
@@ -22,6 +22,7 @@
 #include <avr/wdt.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+#include <avr/cpufunc.h>
 
 #include <util/delay.h>
 
@@ -30,26 +31,35 @@
 #include <limits.h>
 
 
-#define ADC_HYST		2u
-#define ADC_MINMAX_DEADBAND	40u
-#define ADC_REAL_MIN		0u
-#define ADC_REAL_MAX		0x3FFu
-#define ADC_MIN			(ADC_REAL_MIN + ADC_MINMAX_DEADBAND)
-#define ADC_MAX			(ADC_REAL_MAX - ADC_MINMAX_DEADBAND)
-#define ADC_INVERT		true
+/* ADC configuration. */
+#define ADC_HYST		1u		/* ADC hysteresis */
+#define ADC_MINMAX_DEADBAND	10u		/* Deadband at min/max positions */
+#define ADC_REAL_MIN		0u		/* Physical ADC minimum */
+#define ADC_REAL_MAX		0x3FFu		/* Physical ADC maximum */
+#define ADC_MIN			(ADC_REAL_MIN + ADC_MINMAX_DEADBAND) /* Logical ADC minimum */
+#define ADC_MAX			(ADC_REAL_MAX - ADC_MINMAX_DEADBAND) /* Logical ADC maximum */
+#define ADC_INVERT		true		/* Invert ADC signal? */
 
-#define PWM_MIN			0u
-#define PWM_MAX			0xFFu
-#define PWM_NEGLIM		(PWM_MIN + 0u)
-#define PWM_POSLIM		(PWM_MAX - 10u)
-#define PWM_INVERT		false
+/* PWM configuration. */
+#define PWM_MIN			0u		/* Physical PWM minimum */
+#define PWM_MAX			0xFFu		/* Physical PWM maximum */
+#define PWM_NEGLIM		(PWM_MIN + 0u)	/* Logical PWM mimimum */
+#define PWM_POSLIM		(PWM_MAX - 10u)	/* Logical PWM maximum */
+#define PWM_INVERT		false		/* Invert PWM signal? */
+#define PWM_HIGHRES_SP_THRES	6000u		/* High resolution threshold */
+#define PWM_SP_TO_CPU_CYC_MUL	1u		/* Setpoint to cycle multiplicator */
+#define PWM_SP_TO_CPU_CYC_DIV	1u		/* Setpoint to cycle divisor */
 
 
+/* Helper macros. */
 #define abs(x)			((x) >= 0 ? (x) : -(x))
 #define min(a, b)		((a) < (b) ? (a) : (b))
 #define max(a, b)		((a) > (b) ? (a) : (b))
 #define clamp(x, min_x, max_x)	(max(min((x), (max_x)), (min_x)))
 #define ARRAY_SIZE(a)		(sizeof(a) / sizeof((a)[0]))
+
+#define OUTPUT_HIGH		(PWM_INVERT ? false : true)
+#define OUTPUT_LOW		(PWM_INVERT ? true : false)
 
 
 struct curve_point {
@@ -59,8 +69,8 @@ struct curve_point {
 #define CURVE_POINT(_x, _y)	{ .x = (uint16_t)(_x), \
 				  .y = (uint16_t)(_y), }
 
-/* Brightness curve. */
-static const struct curve_point __flash brightness_curve[] = {
+/* Value transformation curve. */
+static const struct curve_point __flash transformation_curve[] = {
 	CURVE_POINT(0,     0),
 	CURVE_POINT(8192,  1562),
 	CURVE_POINT(16384, 3836),
@@ -126,6 +136,27 @@ static uint16_t curve_interpolate(const struct curve_point __flash *curve,
 	return y;
 }
 
+/* Set the PWM output port state. */
+static void port_out_set(bool high)
+{
+	if (high)
+		PORTB |= (1 << PB0);
+	else
+		PORTB &= ~(1 << PB0);
+}
+
+#if PWM_INVERT == false
+# define ASM_PWM_OUT_HIGH	"sbi %[_OUT_PORT], %[_OUT_BIT] \n"
+# define ASM_PWM_OUT_LOW	"cbi %[_OUT_PORT], %[_OUT_BIT] \n"
+#else
+# define ASM_PWM_OUT_HIGH	"cbi %[_OUT_PORT], %[_OUT_BIT] \n"
+# define ASM_PWM_OUT_LOW	"sbi %[_OUT_PORT], %[_OUT_BIT] \n"
+#endif
+
+#define ASM_INPUTS					\
+	[_OUT_PORT]	"I" (_SFR_IO_ADDR(PORTB)),	\
+	[_OUT_BIT]	"M" (PB0)
+
 static void ports_init(void)
 {
 	/* PB0 = output
@@ -140,19 +171,174 @@ static void ports_init(void)
 	PORTB = (1 << PB5) | (0 << PB4) | (1 << PB3) |\
 		(1 << PB2) | (1 << PB1) | ((PWM_INVERT ? 1 : 0) << PB0);
 	/* Wait for pullup input capacity */
-	_delay_ms(50);
+	_delay_ms(20);
+}
+
+/* Active PWM setpoint */
+static uint16_t current_pwm_setpoint;
+
+/* PWM low frequency / high resolution software interrupt handler */
+ISR(TIM0_OVF_vect)
+{
+	uint8_t delay_count8;
+	uint16_t delay_count;
+	uint32_t tmp;
+
+	/* Calculate the duty-cycle-high time duration.
+	 * The calculated value is a CPU delay loop value and thus
+	 * depends on the CPU frequency. */
+	tmp = current_pwm_setpoint;
+	tmp = (tmp * PWM_SP_TO_CPU_CYC_MUL) / PWM_SP_TO_CPU_CYC_DIV;
+	delay_count = clamp(tmp, 0u, UINT16_MAX);
+
+	/* Switch the PWM output high, then delay, then switch the output low.
+	 * (It it Ok to delay for a short time in the this interrupt). */
+	if (delay_count == 0) {
+		/* No delay (off) */
+
+		__asm__ __volatile__ (
+			ASM_PWM_OUT_LOW
+		: : ASM_INPUTS
+		: );
+	} else if (delay_count == 1) {
+		/* 1 clock delay */
+
+		__asm__ __volatile__ (
+			ASM_PWM_OUT_HIGH
+			ASM_PWM_OUT_LOW
+		: : ASM_INPUTS
+		: );
+	} else if (delay_count == 2) {
+		/* 2 clocks delay */
+
+		__asm__ __volatile__ (
+			ASM_PWM_OUT_HIGH
+		"	nop			\n"
+			ASM_PWM_OUT_LOW
+		: : ASM_INPUTS
+		: );
+	} else if (delay_count / 3u <= 255u) {
+		/* 3 clocks per loop iteration
+		 * -> divide count */
+		delay_count8 = delay_count / 3u;
+
+		__asm__ __volatile__ (
+			ASM_PWM_OUT_HIGH
+		"1:	dec %[_delay_count8]	\n"
+		"	brne 1b			\n"
+			ASM_PWM_OUT_LOW
+		: [_delay_count8] "=d" (delay_count8)
+		:                 "0" (delay_count8),
+		  ASM_INPUTS
+		: );
+	} else {
+		/* 4 clocks per loop iteration
+		 * -> divide count */
+		delay_count /= 4u;
+
+		__asm__ __volatile__ (
+			ASM_PWM_OUT_HIGH
+		"1:	sbiw %[_delay_count], 1	\n"
+		"	brne 1b			\n"
+			ASM_PWM_OUT_LOW
+		: [_delay_count] "=w" (delay_count)
+		:                "0" (delay_count),
+		  ASM_INPUTS
+		: );
+	}
+
+	/* We don't want to re-trigger right now,
+	 * just in case the delay took long.
+	 * Clear the interrupt flag. */
+	TIFR0 = (1u << TOV0);
+}
+
+/* Set the PWM setpoint. */
+static void pwm_set(uint16_t setpoint, bool clock_fast, bool irq_mode)
+{
+	uint8_t pwm;
+	uint32_t pwm_range;
+
+	/* Store the setpoint for use in TIM0_OVF interrupt. */
+	current_pwm_setpoint = setpoint;
+
+	/* Calculate PWM value from the setpoint value */
+	pwm_range = PWM_POSLIM - PWM_NEGLIM;
+	pwm = (uint8_t)(((uint32_t)setpoint * pwm_range) / 0xFFFFu);
+	pwm += PWM_NEGLIM;
+
+	/* Invert the PWM, if required. */
+	if (!PWM_INVERT)
+		pwm = PWM_MAX - pwm;
+
+	/* Enable the TIM0_OVF interrupt, if requested. */
+	if (irq_mode) {
+		TIMSK0 |= (1u << TOIE0);
+	} else {
+		TIMSK0 &= ~(1u << TOIE0);
+		TIFR0 = (1u << TOV0);
+	}
+
+	if (irq_mode || pwm == PWM_MIN) {
+		/* In interrupt mode or of the duty cycle is zero,
+		 * do not drive the output pin by hardware. */
+		TCCR0A = (0 << COM0A1) | (0 << COM0A0) |\
+			 (0 << COM0B1) | (0 << COM0B0) |\
+			 (1 << WGM01) | (1 << WGM00);
+	} else {
+		/* Drive the output pin by hardware. */
+		TCCR0A = (1 << COM0A1) | (1 << COM0A0) |\
+			 (0 << COM0B1) | (0 << COM0B0) |\
+			 (1 << WGM01) | (1 << WGM00);
+	}
+
+	/* Set the clock prescaler (fast or slow). */
+	if (clock_fast) {
+		TCCR0B = (0 << FOC0A) | (0 << FOC0B) |\
+			 (0 << WGM02) |\
+			 (0 << CS02) | (0 << CS01) | (1 << CS00);
+	} else {
+		TCCR0B = (0 << FOC0A) | (0 << FOC0B) |\
+			 (0 << WGM02) |\
+			 (1 << CS02) | (0 << CS01) | (0 << CS00);
+	}
+
+	/* If we are in normal PWM mode,
+	 * set the PWM duty cycle in hardware. */
+	if (!irq_mode) {
+		if (pwm == PWM_MIN) {
+			port_out_set(true);
+		} else {
+			port_out_set(false);
+			OCR0A = pwm;
+		}
+	}
+}
+
+/* Initialize the PWM timer. */
+static void pwm_init(void)
+{
+	TCCR0B = 0u;
+	TCNT0 = PWM_INVERT ? 0u : 0xFFu;
+	OCR0A = PWM_INVERT ? 0u : 0xFFu;
+	OCR0B = PWM_INVERT ? 0u : 0xFFu;
+	pwm_set(0u, true, false);
 }
 
 /* ADC conversion complete interrupt service routine */
 ISR(ADC_vect)
 {
-	uint8_t pwm;
-	uint32_t pwm_range;
 	uint32_t adc_range;
 	uint16_t adc;
 	uint16_t scaled;
-	uint16_t transformed;
+	uint16_t setpoint;
 	static uint16_t prev_adc = ADC_REAL_MIN;
+
+	/* Disable the ADC interrupt and
+	 * globally enable interrupts.
+	 * This allows TIM0_OVF_vect to interrupt us. */
+	ADCSRA &= ~(1u << ADIE);
+	sei();
 
 	/* Read the analog input */
 	adc = ADC;
@@ -173,32 +359,32 @@ ISR(ADC_vect)
 	adc -= ADC_MIN;
 	scaled = (uint16_t)(((uint32_t)adc * 0xFFFFu) / adc_range);
 
-	/* Transform the value according to the brightness curve. */
-	transformed = curve_interpolate(brightness_curve,
-					ARRAY_SIZE(brightness_curve),
-					scaled);
+	/* Transform the value according to the transformation curve. */
+	setpoint = curve_interpolate(transformation_curve,
+				     ARRAY_SIZE(transformation_curve),
+				     scaled);
 
-	/* Calculate PWM value from the transformed ADC value */
-	pwm_range = PWM_POSLIM - PWM_NEGLIM;
-	pwm = (uint8_t)(((uint32_t)transformed * pwm_range) / 0xFFFFu);
-	pwm += PWM_NEGLIM;
+	/* Globally disable interrupts.
+	 * and re-enable the ADC interrupt.
+	 * TIM0_OVF_vect must not interrupt re-programming of the PWM below. */
+	cli();
+	ADCSRA |= (1u << ADIF) | (1u << ADIE);
 
-	/* Write PWM setpoint */
-	if (!PWM_INVERT)
-		pwm = PWM_MAX - pwm;
-	if (pwm == PWM_MIN) {
-		TCCR0A &= ~((1 << COM0A1) | (1 << COM0A0));
-		PORTB |= (1 << PB0);
+	if (setpoint <= PWM_HIGHRES_SP_THRES) {
+		/* Small PWM duty cycles are handled with a much
+		 * much higher resolution, but with much lower frequency. */
+		pwm_set(setpoint, false, true);
 	} else {
-		TCCR0A |= (1 << COM0A1) | (1 << COM0A0);
-		PORTB &= ~(1 << PB0);
-		OCR0A = pwm;
+		/* Normal PWM duty cycle.
+		 * Use high frequency low resolution PWM. */
+		pwm_set(setpoint, true, false);
 	}
 
 	/* Poke the watchdog */
 	wdt_reset();
 }
 
+/* Initialize the input ADC measurement. */
 static void adc_init(void)
 {
 	/* Disable ADC2 digital input */
@@ -218,21 +404,7 @@ static void adc_init(void)
 		  (1 << ADIF) | (1 << ADIE);
 }
 
-static void pwm_init(void)
-{
-	/* Fast PWM; Prescaler 256 */
-	TCCR0B = 0;
-	TCNT0 = PWM_INVERT ? 0 : 0xFF;
-	OCR0A = PWM_INVERT ? 0 : 0xFF;
-	OCR0B = PWM_INVERT ? 0 : 0xFF;
-	TCCR0A = (1 << COM0A1) | (1 << COM0A0) |\
-		 (0 << COM0B1) | (0 << COM0B0) |\
-		 (1 << WGM01) | (1 << WGM00);
-	TCCR0B = (0 << FOC0A) | (0 << FOC0B) |\
-		 (0 << WGM02) |\
-		 (0 << CS02) | (0 << CS01) | (1 << CS00);
-}
-
+/* Early watchdog timer initialization. */
 void wdt_early_init(void) __attribute__((naked, used, section(".init3")));
 void wdt_early_init(void)
 {
@@ -240,6 +412,7 @@ void wdt_early_init(void)
 	wdt_enable(WDTO_500MS);
 }
 
+/* Main program entry point. */
 int main(void)
 {
 	ports_init();
