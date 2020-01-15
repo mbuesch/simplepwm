ba6c696935d89f37ec15a2ed444f54ead0c18db2
Michael Buesch <m@bues.ch>
2020-01-15 21:24:35 +0100

    util: Add type safe min/max/abs
    Signed-off-by: Michael Buesch <m@bues.ch>

--- firmware/util.h
+++ firmware/util.h
@@ -5,10 +5,38 @@
 #include <stdbool.h>
 #include <limits.h>
 
-#define abs(x)			((x) >= 0 ? (x) : -(x))
-#define min(a, b)		((a) < (b) ? (a) : (b))
-#define max(a, b)		((a) > (b) ? (a) : (b))
+
+/* Return the smaller value of 'a' and 'b'. */
+#define min(a, b)	({						\
+		__typeof__(a) __amin = (a);				\
+		__typeof__(b) __bmin = (b);				\
+		(__typeof__(a))(__amin < __bmin ? __amin : __bmin);	\
+	})
+
+/* Return the bigger value of 'a' and 'b'. */
+#define max(a, b)	({						\
+		__typeof__(a) __amax = (a);				\
+		__typeof__(b) __bmax = (b);				\
+		(__typeof__(a))(__amax > __bmax ? __amax : __bmax);	\
+	})
+
+/* Return 'value' clamped inbetween 'min_val' and 'max_val'. */
+#define clamp(value, min_val, max_val)		\
+	max(min(value, max_val), min_val)
+
+/* Return the absolute value of 'val' */
+#undef abs
+#define abs(val)	({			\
+		__typeof__(val) __val = (val);	\
+		__val >= 0 ? __val : -__val;	\
+	})
+
+
+/* Get the number of elements in a C array. */
 #define ARRAY_SIZE(a)		(sizeof(a) / sizeof((a)[0]))
+
+/* Memory barrier. */
 #define memory_barrier()	__asm__ __volatile__("" : : : "memory")
+
 
 #endif /* UTIL_H_ */
