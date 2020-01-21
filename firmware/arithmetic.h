b94a89cf799db18a758c2717cbae7abe39b530e5
Michael Buesch <m@bues.ch>
2020-01-21 21:55:55 +0100

    arithmetic: Add saturating subtraction
    Signed-off-by: Michael Buesch <m@bues.ch>

--- firmware/arithmetic.h
+++ firmware/arithmetic.h
@@ -7,7 +7,7 @@
 #define ARITHMETIC_ASM_AVR8	1
 
 
-/* Saturated unsigned add (32 bit). */
+/* Saturated unsigned addition (32 bit). */
 static inline uint32_t add_sat_u32(uint32_t a, uint32_t b)
 {
 #if ARITHMETIC_ASM_AVR8
@@ -19,8 +19,9 @@ static inline uint32_t add_sat_u32(uint32_t a, uint32_t b)
 		"brcc 1f \n"
 		"ldi %A0, 0xFF \n"
 		"ldi %B0, 0xFF \n"
-		"ldi %C0, 0xFF \n"
-		"ldi %D0, 0xFF \n"
+		"movw %C0, %A0 \n"
+/*		"ldi %C0, 0xFF \n"
+		"ldi %D0, 0xFF \n" */
 		"1: \n"
 		: "+d" (a)
 		: "d" (b)
@@ -35,7 +36,7 @@ static inline uint32_t add_sat_u32(uint32_t a, uint32_t b)
 }
 
 #ifdef uint24_t
-/* Saturated unsigned add (24 bit). */
+/* Saturated unsigned addition (24 bit). */
 static inline uint24_t add_sat_u24(uint24_t a, uint24_t b)
 {
 #if ARITHMETIC_ASM_AVR8
@@ -61,7 +62,7 @@ static inline uint24_t add_sat_u24(uint24_t a, uint24_t b)
 }
 #endif /* uint24_t */
 
-/* Saturated unsigned add (16 bit). */
+/* Saturated unsigned addition (16 bit). */
 static inline uint16_t add_sat_u16(uint16_t a, uint16_t b)
 {
 #if ARITHMETIC_ASM_AVR8
@@ -84,7 +85,7 @@ static inline uint16_t add_sat_u16(uint16_t a, uint16_t b)
 #endif
 }
 
-/* Saturated unsigned add (8 bit). */
+/* Saturated unsigned addition (8 bit). */
 static inline uint8_t add_sat_u8(uint8_t a, uint8_t b)
 {
 #if ARITHMETIC_ASM_AVR8
@@ -101,6 +102,105 @@ static inline uint8_t add_sat_u8(uint8_t a, uint8_t b)
 	uint8_t x = (uint8_t)(a + b);
 	if (x < a)
 		return UINT8_MAX;
+	return x;
+#endif
+}
+
+/* Saturated unsigned subtraction (32 bit). */
+static inline uint32_t sub_sat_u32(uint32_t a, uint32_t b)
+{
+#if ARITHMETIC_ASM_AVR8
+	__asm__ __volatile__(
+		"sub %A0, %A1 \n"
+		"sbc %B0, %B1 \n"
+		"sbc %C0, %C1 \n"
+		"sbc %D0, %D1 \n"
+		"brcc 1f \n"
+		"clr %A0 \n"
+		"clr %B0 \n"
+		"movw %C0, %A0 \n"
+/*		"clr %C0 \n"
+		"clr %D0 \n" */
+		"1: \n"
+		: "+r" (a)
+		: "r" (b)
+	);
+	return a;
+#else
+	uint32_t x = (uint32_t)(a - b);
+	if (x > a)
+		return 0;
+	return x;
+#endif
+}
+
+#ifdef uint24_t
+/* Saturated unsigned subtraction (24 bit). */
+static inline uint24_t sub_sat_u24(uint24_t a, uint24_t b)
+{
+#if ARITHMETIC_ASM_AVR8
+	__asm__ __volatile__(
+		"sub %A0, %A1 \n"
+		"sbc %B0, %B1 \n"
+		"sbc %C0, %C1 \n"
+		"brcc 1f \n"
+		"clr %A0 \n"
+		"clr %B0 \n"
+		"clr %C0 \n"
+		"1: \n"
+		: "+r" (a)
+		: "r" (b)
+	);
+	return a;
+#else
+	uint24_t x = (uint24_t)(a - b);
+	if (x > a)
+		return 0;
+	return x;
+#endif
+}
+#endif /* uint24_t */
+
+/* Saturated unsigned subtraction (16 bit). */
+static inline uint16_t sub_sat_u16(uint16_t a, uint16_t b)
+{
+#if ARITHMETIC_ASM_AVR8
+	__asm__ __volatile__(
+		"sub %A0, %A1 \n"
+		"sbc %B0, %B1 \n"
+		"brcc 1f \n"
+		"clr %A0 \n"
+		"clr %B0 \n"
+		"1: \n"
+		: "+r" (a)
+		: "r" (b)
+	);
+	return a;
+#else
+	uint16_t x = (uint16_t)(a - b);
+	if (x > a)
+		return 0;
+	return x;
+#endif
+}
+
+/* Saturated unsigned subtraction (8 bit). */
+static inline uint8_t sub_sat_u8(uint8_t a, uint8_t b)
+{
+#if ARITHMETIC_ASM_AVR8
+	__asm__ __volatile__(
+		"sub %0, %1 \n"
+		"brcc 1f \n"
+		"clr %0 \n"
+		"1: \n"
+		: "+r" (a)
+		: "r" (b)
+	);
+	return a;
+#else
+	uint8_t x = (uint8_t)(a - b);
+	if (x > a)
+		return 0;
 	return x;
 #endif
 }
