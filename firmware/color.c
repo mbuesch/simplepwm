d53129aa1c720a111fd8f435249e2fda75199ad1
Michael Buesch <m@bues.ch>
2020-06-30 20:25:26 +0200

    color: Add float implementation
    Signed-off-by: Michael Buesch <m@bues.ch>

--- firmware/color.c
+++ firmware/color.c
@@ -22,16 +22,32 @@
 #include "color.h"
 #include "util.h"
 
+#ifndef USE_FLOAT
+# define USE_FLOAT	0
+#endif
+
 
 /* Fractions of the value range. */
-#define f1_1	((int32_t)UINT16_MAX + 1)
-#define f1_6	(f1_1 / 6)
-#define f1_3	(f1_1 / 3)
-#define f1_2	(f1_1 / 2)
-#define f2_3	((f1_1 * 2) / 3)
-#define f6_1	(f1_1 * 6)
+#if USE_FLOAT
+# define f0_x	((float)(0.0f))
+# define f1_1	((float)(1.0f))
+# define f1_6	((float)(1.0f / 6.0f))
+# define f1_3	((float)(1.0f / 3.0f))
+# define f1_2	((float)(1.0f / 2.0f))
+# define f2_3	((float)(2.0f / 3.0f))
+# define f6_1	((float)(6.0f))
+#else
+# define f0_x	((int32_t)0)
+# define f1_1	((int32_t)UINT16_MAX + 1)
+# define f1_6	(f1_1 / 6)
+# define f1_3	(f1_1 / 3)
+# define f1_2	(f1_1 / 2)
+# define f2_3	((f1_1 * 2) / 3)
+# define f6_1	(f1_1 * 6)
+#endif
 
 
+#if !USE_FLOAT
 static int32_t mulq16(int32_t a, int32_t b)
 {
 	int64_t x;
@@ -42,13 +58,41 @@ static int32_t mulq16(int32_t a, int32_t b)
 
 	return (int32_t)x;
 }
+#endif
 
+#if USE_FLOAT
+static uint16_t h2rgb(float x, float y, float h)
+{
+	float ret;
+
+	/* modulo */
+	if (h < f0_x)
+		h += f1_1;
+	if (h > f1_1)
+		h -= f1_1;
+
+	if (h < f1_6)
+		ret = x + ((y - x) * (h * f6_1));
+	else if (h < f1_2)
+		ret = y;
+	else if (h < f2_3)
+		ret = x + ((y - x) * ((f2_3 - h) * f6_1));
+	else
+		ret = x;
+
+	if (ret < f0_x)
+		return 0u;
+	if (ret > f1_1)
+		return UINT16_MAX;
+	return (uint16_t)(ret * (float)UINT16_MAX);
+}
+#else /* USE_FLOAT */
 static uint16_t h2rgb(int32_t x, int32_t y, int32_t h)
 {
 	int32_t ret;
 
 	/* modulo */
-	if (h < 0)
+	if (h < f0_x)
 		h += f1_1;
 	if (h > f1_1)
 		h -= f1_1;
@@ -64,11 +108,34 @@ static uint16_t h2rgb(int32_t x, int32_t y, int32_t h)
 
 	return lim_u16(ret);
 }
+#endif /* USE_FLOAT */
 
 /* Convert from HSL color model to RGB. */
 void hsl2rgb(uint16_t *r, uint16_t *g, uint16_t *b,
 	     uint16_t h, uint16_t s, uint16_t l)
 {
+#if USE_FLOAT
+	float hh, ss, ll, x, y;
+
+	if (s == 0u) {
+		*r = *g = *b = l;
+	} else {
+		hh = (float)h * (f1_1 / (float)UINT16_MAX);
+		ss = (float)s * (f1_1 / (float)UINT16_MAX);
+		ll = (float)l * (f1_1 / (float)UINT16_MAX);
+
+		if (ll <= f1_2)
+			y = ll + (ll * ss);
+		else
+			y = (ll + ss) - (ll * ss);
+		x = (ll + ll) - y;
+
+		*r = h2rgb(x, y, hh + f1_3);
+		*g = h2rgb(x, y, hh);
+		*b = h2rgb(x, y, hh - f1_3);
+	}
+
+#else /* USE_FLOAT */
 	int32_t x, y;
 
 	if (s == 0u) {
@@ -84,4 +151,5 @@ void hsl2rgb(uint16_t *r, uint16_t *g, uint16_t *b,
 		*g = h2rgb(x, y, h);
 		*b = h2rgb(x, y, (int32_t)h - f1_3);
 	}
+#endif /* USE_FLOAT */
 }
--- firmware/tests/test_color.py
+++ firmware/tests/test_color.py
@@ -6,18 +6,23 @@ from unittest import TestCase
 from colorsys import hls_to_rgb
 
 with contextlib.suppress(OSError):
-	os.unlink("test_color.bin")
-run(["cc", "-O2", "-o", "test_color.bin", "test_color.c"])
+	os.unlink("test_color_int.bin")
+with contextlib.suppress(OSError):
+	os.unlink("test_color_float.bin")
+run(["cc", "-O2", "-DUSE_FLOAT=0", "-o", "test_color_int.bin", "test_color.c"])
+run(["cc", "-O2", "-DUSE_FLOAT=1", "-o", "test_color_float.bin", "test_color.c"])
 
 class Color_Test(TestCase):
-	def test_0(self):
+	def __runtest(self, isfloat):
 		def t(h, s, l):
+			testbin = "test_color_float.bin" if isfloat else "test_color_int.bin"
 			res = (int(x)
-			       for x in run(["./test_color.bin", str(h), str(s), str(l)],
+			       for x in run(["./" + testbin, str(h), str(s), str(l)],
 			       		    stdout=PIPE).stdout.decode("UTF-8").split())
 			exp = (round(x * 0xFFFF)
 			       for x in hls_to_rgb(h / 0xFFFF, l / 0xFFFF, s / 0xFFFF))
-			if not all( abs(a - b) <= 6 for a, b in zip(res, exp) ):
+			thres = 1 if isfloat else 6
+			if not all( abs(a - b) <= thres for a, b in zip(res, exp) ):
 				print("H = %X   S = %X   L = %X" % (h, s, l))
 				print("res =", list(res), ", exp =", list(exp))
 				assert 0, "Invalid RGB conversion."
@@ -26,3 +31,9 @@ class Color_Test(TestCase):
 			for s in range(0, 0xFFFF+1, 0x1000):
 				for l in range(0, 0xFFFF+1, 0x1000):
 					t(h, s, l)
+
+	def test_float(self):
+		self.__runtest(isfloat=True)
+
+	def test_int(self):
+		self.__runtest(isfloat=False)
